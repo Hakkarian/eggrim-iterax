f7a6a7f3018a935c164b3f5e5e01d93e02dd1b6c
Hakkarian <theobadar@gmail.com>
2026-09-12 22:37:11 +0300

    [EI-15] legacy procedure sprites removal

--- eggrim/app.py
+++ eggrim/app.py
@@ -563,43 +563,6 @@ def draw():
             h,
             0,
         )
-    elif view in (Facing.LEFT, Facing.RIGHT):
-        walk_frame = (0, 1, 2, 1)[player.walk_phase % 20 // 5]
-        if attacking:
-            if thrust.anim == THRUST_ANIM_FRAMES or thrust.anim == 1:
-                pose = 3
-            elif thrust.anim == THRUST_ANIM_FRAMES - 1 or thrust.anim == 2:
-                pose = 4
-            else:
-                pose = 5
-        else:
-            pose = walk_frame
-        frames = ((0, 0), (48, 32), (0, 32), (96, 32), (144, 32), (0, 48))
-        sprite_u, sprite_v = frames[pose]
-        sprite_w = -16 if view is Facing.LEFT else 16
-        pyxel.blt(
-            int(player.x) - 8,
-            int(player.y) - 8 - bob,
-            0,
-            sprite_u,
-            sprite_v,
-            sprite_w,
-            16,
-            0,
-        )
-    else:
-        front = direction == "front"
-        sprite_u, sprite_v = (16, 0) if front else (0, 16)
-        pyxel.blt(
-            int(player.x) - 8,
-            int(player.y) - 8 - bob,
-            0,
-            sprite_u,
-            sprite_v,
-            16,
-            16,
-            0,
-        )
     if attacking:
         draw_strike(view)
     for pillar in pillars:
--- eggrim/assets/__init__.py
+++ eggrim/assets/__init__.py
@@ -12,28 +12,6 @@ from eggrim.assets.pillar import (
     PILLAR_CHARS,
     render_pillar_flash,
 )
-from eggrim.assets.player_sprites import (
-    BACK_ATTACK_CHARS,
-    BACK_BLEND_CHARS,
-    BACK_FADE_CHARS,
-    BACK_HALF_CHARS,
-    BACK_CHARS,
-    BACK_ROWS,
-    BACK_WALK_CHARS,
-    FRONT_ATTACK_CHARS,
-    FRONT_BLEND_CHARS,
-    FRONT_CHARS,
-    FRONT_FADE_CHARS,
-    FRONT_HALF_CHARS,
-    FRONT_ROWS,
-    FRONT_WALK_CHARS,
-    SIDE_ATTACK_CHARS,
-    SIDE_BLEND_CHARS,
-    SIDE_FADE_CHARS,
-    SIDE_HALF_CHARS,
-    SIDE_WALK_CHARS,
-    SPRITE_CHARS,
-)
 from eggrim.assets.portraits import (
     PORTRAIT_BLEND_POS,
     PORTRAIT_THUMB_POS,
@@ -44,25 +22,7 @@ from eggrim.assets.portraits import (
 
 def load_banks():
     palette_ready = read_palette()
-    pyxel.images[0].set(0, 0, SPRITE_CHARS)
-    pyxel.images[0].set(16, 0, FRONT_CHARS)
-    pyxel.images[0].set(0, 16, BACK_CHARS)
     pyxel.images[0].set(128, 0, PILLAR_CHARS)
-    pyxel.images[0].set(0, 48, SIDE_ATTACK_CHARS)
-    pyxel.images[0].set(16, 48, FRONT_ATTACK_CHARS)
-    pyxel.images[0].set(32, 48, BACK_ATTACK_CHARS)
-    pyxel.images[0].set(0, 32, SIDE_WALK_CHARS)
-    pyxel.images[0].set(16, 32, FRONT_WALK_CHARS)
-    pyxel.images[0].set(32, 32, BACK_WALK_CHARS)
-    pyxel.images[0].set(48, 32, SIDE_HALF_CHARS)
-    pyxel.images[0].set(64, 32, FRONT_HALF_CHARS)
-    pyxel.images[0].set(80, 32, BACK_HALF_CHARS)
-    pyxel.images[0].set(96, 32, SIDE_BLEND_CHARS)
-    pyxel.images[0].set(112, 32, FRONT_BLEND_CHARS)
-    pyxel.images[0].set(128, 32, BACK_BLEND_CHARS)
-    pyxel.images[0].set(144, 32, SIDE_FADE_CHARS)
-    pyxel.images[0].set(160, 32, FRONT_FADE_CHARS)
-    pyxel.images[0].set(176, 32, BACK_FADE_CHARS)
     render_pillar_flash()
     render_thumbs()
     render_portrait_blends()
